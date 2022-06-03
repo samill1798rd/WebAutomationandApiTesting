ea9ef4c7c66a5f9aceecb7afb9ead8289672db69
Gilbert Samill Rivas <gilbert.rivasrd@gmail.com>
2022-06-03 18:55:14 -0400

    Wait remove from homepage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -28,7 +28,6 @@ public class HomePage {
 
   public void scrollDownToLeftItem() {
     utils.runJavascriptExecutor("window.scrollBy(0,800)");
-    utils.wait(5);
   }
 
   public void scrollDownToFooter() {
